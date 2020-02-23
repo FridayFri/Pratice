68bc621467c1532bf0f38370e29eb539e705584a
Friday <1134738416@qq.com>
2020-02-23 18:31:30 +0800

    3最长子串

---
+++ 3.lengthOfLongestSubstring.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let array = [];
+  let max = 0;
+  for (let item of s) {
+    if (array.includes(item)) {
+      console.log("item", item);
+      let index = array.indexOf(item);
+      console.log(index);
+      array.splice(0, index + 1);
+    }
+    array.push(item);
+    console.log('max', max)
+    max = max > array.length ? max : array.length;
+  }
+  return max;
+};
+console.log(lengthOfLongestSubstring("assdfdsafsafa"));
