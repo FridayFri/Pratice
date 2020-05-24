205d9e5c415e94f69116ac02b3efb93dd4fa04e3
shengqiwu <shengqiwu@guanmai.cn>
2020-05-25 00:02:50 +0800

    209

--- 209.minSubArrayLen.js
+++ 209.minSubArrayLen.js
@@ -1,15 +1,16 @@
-var minSubArrayLen = function(s, nums) {
-    const int_max = Number.MAX_SAFE_INTEGER
-    var i = 0, sum = 0, ans = int_max
-    for (var j = 0; j < nums.length; j++) {
-        sum += nums[j]
-        while (sum >= s) {
-            ans = Math.min(ans, j - i + 1)
-            sum -= nums[i++]
-        }
+var minSubArrayLen = function (s, nums) {
+  let max = Number.MAX_SAFE_INTEGER;
+  let i = 0;
+  sum = 0;
+  for (let j = 0; j < nums.length; j++) {
+    sum += nums[j];
+    while (sum >= s) {
+      max = Math.min(max, j - i + 1);
+      sum -= nums[i++];
     }
-    return ans === int_max ? 0 : ans
-}
+  }
+  return  max === Number.MAX_SAFE_INTEGER ? 0 : max;
+};
 var s = 3;
-var nums = [1,1];
+var nums = [1, 1];
 console.log(minSubArrayLen(s, nums));
