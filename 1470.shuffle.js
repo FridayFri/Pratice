167f3ede23d195c3f23682c6c7542ebaeddb6a49
shengqiwu <shengqiwu@guanmai.cn>
2020-08-07 00:51:47 +0800

    1470

--- 1470.shuffle.js
+++ 1470.shuffle.js
@@ -1,10 +1,10 @@
 var shuffle = function (nums, n) {
-    const result = []
-    for(let i = 0;i<n;i++){
-        result.push(nums[i])
-        result.push(nums[n+i])
-    }
-    return result
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums[i]);
+    result.push(nums[n + i]);
+  }
+  return result;
 };
 
 const nums = [2, 5, 1, 3, 4, 7],
