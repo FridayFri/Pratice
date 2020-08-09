fad6e99507899f07315a2cec7539f226f48c9144
shengqiwu <shengqiwu@guanmai.cn>
2020-08-09 19:04:44 +0800

    152

---
+++ 152.maxProduct.js
@@ -0,0 +1,16 @@
+var maxProduct = function(nums) {
+    let max = nums[0];
+    let min = nums[0];
+    let res = nums[0];
+  
+    for (let i = 1; i < nums.length; i++) {
+      let tmp = min;
+      min = Math.min(nums[i], Math.min(max * nums[i], min * nums[i])); // 取最小
+      max = Math.max(nums[i], Math.max(max * nums[i], tmp * nums[i])); /// 取最大
+      res = Math.max(res, max);
+    }
+    return res;
+  };
+
+const nums = [2, 3, -2, 4];
+console.log(maxProduct(nums));
