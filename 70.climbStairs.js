0e2ec1fc13232df4a93b19e73eb3fda93e08dc1f
shengqiwu <shengqiwu@guanmai.cn>
2020-08-12 02:00:24 +0800

    70

--- 70.climbStairs.js
+++ 70.climbStairs.js
@@ -2,22 +2,17 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-  if (n === 1) {
-    return 1;
+var climbStairs = function (n) {
+  // if (n <= 2) {
+  //   return n;
+  // }
+  const dp = [];
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
-  if (n === 2) {
-    return 2;
-  }
-  let a = 1,
-    b = 2;
-  let temp;
-  for (let i = 3; i <= n; i++) {
-    temp = a;
-    a = b;
-    b = temp + b;
-  }
-  return b
+  return dp[n];
 };
 
 console.log(climbStairs(10));
