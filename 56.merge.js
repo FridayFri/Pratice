d52511d0e8f1e21c8db42b7dafd38bd972bfa731
shengqiwu <shengqiwu@guanmai.cn>
2020-08-13 16:59:50 +0800

    56

--- 56.merge.js
+++ 56.merge.js
@@ -1,26 +1,11 @@
-var merge = function (intervals) {
-  if (intervals.length == 0) return [];
-  var res = [];
-  intervals.sort(function (a, b) {
-    return a[0] - b[0];
-  });
-  res.push(intervals[0]);
-  for (var i = 1; i < intervals.length; i++) {
-    if (intervals[i][0] > res[res.length - 1][1]) res.push(intervals[i]);
-    else if (intervals[i][1] > res[res.length - 1][1])
-      res[res.length - 1][1] = intervals[i][1];
-  }
-  return res;
-};
 
 const intervals = [
   [1, 4],
   [0, 4],
 ];
-// console.log(merge(intervals));
-console.log(merges(intervals));
+console.log(merge(intervals));
 
-function merges() {
+function merge() {
   if (intervals.length == 0) return [];
   intervals.sort(function (a, b) {
     console.log(a, b);
