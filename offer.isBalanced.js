f4a43f87df0d9c5022418dd993aa47b197a9d799
shengqiwu <shengqiwu@guanmai.cn>
2020-09-06 18:50:30 +0800

    isBalanced

---
+++ offer.isBalanced.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) {
+    return true;
+  }
+  const left = dfs(root.left);
+  const right = dfs(root.right);
+  if (Math.abs(left - right) > 1) {
+    return false;
+  }
+  return isBalanced(root.left) && isBalanced(root.right);
+};
+
+var dfs = function (root) {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(dfs(root.left), dfs(root.right)) + 1;
+};
+
+const root = {
+    val: 3,
+    left: { val: 9, left: null, right: null },
+    right: {
+      val: 20,
+      left: { val: 15, left: null, right: null },
+      right: { val: 7, left: null, right: null },
+    },
+  };
+console.log(isBalanced(root))
