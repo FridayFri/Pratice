0f4e20625a451b56d50ecc06cf1769bfc7191e9b
shengqiwu <shengqiwu@guanmai.cn>
2020-09-08 00:46:48 +0800

    965

---
+++ 965.isUnivalTree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function (root) {
+  let queue = [root];
+
+  while (queue.length) {
+    let node = queue.shift();
+    if (node.val !== root.val) return 0;
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+
+  return 1;
+};
