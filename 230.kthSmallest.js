d00e4440b26f86d4690d96210ea51d288675f806
shengqiwu <shengqiwu@guanmai.cn>
2020-09-08 13:08:37 +0800

    230

---
+++ 230.kthSmallest.js
@@ -0,0 +1,24 @@
+var kthSmallest = function (root, k) {
+  const arr = bst(root).sort((a, b) => a - b);
+  return arr[k - 1];
+};
+
+var bst = function (root, val = []) {
+  if (root) {
+    bst(root.left, val);
+    val.push(root.val);
+    bst(root.right, val);
+  }
+  return val;
+};
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+const root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+console.log(kthSmallest(root, 3));
