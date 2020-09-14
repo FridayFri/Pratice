b639117fada808dc7b79a3fbdec8907377a0d03a
shengqiwu <shengqiwu@guanmai.cn>
2020-09-15 01:08:03 +0800

    236

---
+++ 236.lowestCommonAncestor.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if(root == null || root == p || root == q) return root
+    const left = lowestCommonAncestor(root.left, p, q)
+    const right = lowestCommonAncestor(root.right, p, q)
+    if(left === null) return right
+    if(right === null) return left
+    return root
+};
+
+// 建树
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+const root = new TreeNode(3);
+root.left = new TreeNode(5);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(6);
+root.left.right = new TreeNode(2);
+root.right.left = new TreeNode(0);
+root.right.right = new TreeNode(8);
+const p = new TreeNode(5);
+const q = new TreeNode(1);
+console.log(lowestCommonAncestor(root, p, q));
