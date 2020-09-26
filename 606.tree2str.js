ff706fb2919518f624452e6599caca63d6cf2fd2
shengqiwu <shengqiwu@guanmai.cn>
2020-09-26 23:50:25 +0800

    606

---
+++ 606.tree2str.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t
+ * @return {string}
+ */
+var tree2str = function (t) {
+  if (!t) {
+    return "";
+  }
+  var rightStr = "";
+  if (t.right) {
+    rightStr = `(${tree2str(t.right)})`;
+  }
+  var leftStr = `(${tree2str(t.left)})`;
+  if (!t.right && leftStr === "()") {
+    leftStr = "";
+  }
+  return `${t.val.toString()}${leftStr}${rightStr}`;
+};
+
+const root = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+  },
+};
+
+console.log(tree2str(root));
