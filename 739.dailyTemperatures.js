af84b2c7cea86eb81dd8a03fc576c86210f13a5d
shengqiwu <shengqiwu@guanmai.cn>
2020-10-13 01:12:26 +0800

    739

--- 739.dailyTemperatures.js
+++ 739.dailyTemperatures.js
@@ -1,14 +1,28 @@
+// var dailyTemperatures = function (T) {
+//   const res = new Array(T.length).fill(0);
+//   for (let i = 0; i < T.length; i++) {
+//     for (let j = i + 1; j < T.length; j++) {
+//       if (T[j] > T[i]) {
+//         res[i] = j - i;
+//         break;
+//       }
+//     }
+//   }
+//   return res
+// };
+
 var dailyTemperatures = function (T) {
   const res = new Array(T.length).fill(0);
-  for (let i = 0; i < T.length; i++) {
-    for (let j = i + 1; j < T.length; j++) {
-      if (T[j] > T[i]) {
-        res[i] = j - i;
-        break;
-      }
+  const stack = [];
+  for (let i = T.length - 1; i >= 0; i--) {
+    while (stack.length && T[i] >= T[stack[stack.length - 1]]) {
+      stack.pop();
     }
+    if (stack.length) {
+      res[i] = stack[stack.length - 1] - i;
+    }
+    stack.push(i);
   }
-  return res
+  return res;
 };
-
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
