3bdce5b91889390e1bf0ef55baa062754da52463
shengqiwu <shengqiwu@guanmai.cn>
2020-10-16 01:27:21 +0800

    1561

---
+++ 1561.maxCoins.js
@@ -0,0 +1,14 @@
+var maxCoins = function (piles) {
+  piles.sort((a, b) => b - a);
+  let k = piles.length / 3;
+  let res = 0;
+  for (let i = 0; i < k; i++) {
+    res += piles[i * 2 + 1];
+    console.log(res);
+  }
+  return res;
+};
+
+console.log(
+  maxCoins((piles = [4, 4, 17, 7, 16, 16, 16, 15, 2, 3, 1, 17, 6, 12, 9]))
+);
