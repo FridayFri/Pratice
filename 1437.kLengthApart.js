b67075ec24700370a9946c72250c0ff7741b23fa
shengqiwu <shengqiwu@guanmai.cn>
2020-10-21 22:42:26 +0800

    1437-2

--- 1437.kLengthApart.js
+++ 1437.kLengthApart.js
@@ -1,18 +1,38 @@
+// var kLengthApart = function (nums, k) {
+//   let start = 0,
+//     end = 0,
+//     res = true;
+//   for (let i = 0; i < nums.length; i++) {
+//     end = i;
+//     if (nums[i] === 1 && i > 1) {
+//       console.log(start, end);
+//       if (end - start <= k) {
+//         res = false;
+//       }
+//       start = i;
+//     }
+//   }
+//   return res;
+// };
 var kLengthApart = function (nums, k) {
-  let start = 0,
-    end = 0,
-    res = true;
-  for (let i = 0; i < nums.length; i++) {
-    end = i;
-    if (nums[i] === 1 && i > 1) {
-      console.log(start, end);
-      if (end - start <= k) {
-        res = false;
-      }
-      start = i;
+  let indexArr = [],
+    n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] === 1) {
+      indexArr.push(i);
     }
   }
-  return res;
+
+  if (indexArr.length < 2) return true;
+
+  for (let i = 1; i < indexArr.length; i++) {
+    if (indexArr[i] - indexArr[i - 1] <= k) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 console.log(kLengthApart((nums = [0, 1, 0, 1]), (k = 1)));
