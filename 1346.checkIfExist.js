7513de3d99467f9ed26c561b135b52367203f2f6
shengqiwu <shengqiwu@guanmai.cn>
2020-10-23 00:04:41 +0800

    1346

--- 1346.checkIfExist.js
+++ 1346.checkIfExist.js
@@ -1,7 +1,8 @@
 var checkIfExist = function (arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
+    for (let j = i+1; j < arr.length; j++) {
       if (arr[i] === 2 * arr[j] || arr[j] === 2 * arr[i]) {
+        console.log(arr[i], arr[j]);
         return true;
       }
     }
@@ -9,4 +10,4 @@ var checkIfExist = function (arr) {
   return false;
 };
 
-console.log(checkIfExist((arr = [7, 1, 14, 11])));
+console.log(checkIfExist((arr = [0, 0])));
