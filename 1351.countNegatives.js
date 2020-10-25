e3f118d79dbc05aad2f9e6615c79bcf192d194cf
shengqiwu <shengqiwu@guanmai.cn>
2020-10-26 00:43:14 +0800

    1351-2

--- 1351.countNegatives.js
+++ 1351.countNegatives.js
@@ -2,14 +2,35 @@
  * @param {number[][]} grid
  * @return {number}
  */
+// var countNegatives = function (grid) {
+//   let res = 0;
+//   for (let i = 0; i < grid.length; i++) {
+//     for (let j = 0; j < grid[0].length; j++) {
+//       if (grid[i][j] < 0) {
+//         res++;
+//       }
+//     }
+//   }
+//   return res;
+// };
+
 var countNegatives = function (grid) {
   let res = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] < 0) {
-        res++;
+  const m = grid.length;
+  const n = grid[0].length;
+  for (let i = 0; i < m; i++) {
+    let row = grid[i];
+    let left = 0;
+    let right = n - 1;
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (row[mid] < 0) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
+    res += n - left;
   }
   return res;
 };
