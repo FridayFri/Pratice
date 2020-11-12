7a7dd62adba2eb056338972125ae0bf7ee445c7f
shengqiwu <shengqiwu@guanmai.cn>
2020-11-13 00:19:39 +0800

    offer24

--- leetcode/offer24.reverseList.js
+++ leetcode/offer24.reverseList.js
@@ -9,26 +9,40 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var reverseList = function (head) {
+//   console.log("head", head);
+//   const stack = [];
+//   while (head) {
+//     stack.push(head.val);
+//     head = head.next;
+//   }
+//   while (stack.length) {
+//     if (!head) {
+//       head = new ListNode(stack.pop());
+//     } else {
+//       let p = head;
+//       while (p.next !== null) {
+//         p = p.next;
+//       }
+//       p.next = new ListNode(stack.pop());
+//     }
+//   }
+//   return head;
+// };
+
 var reverseList = function (head) {
-  console.log("head", head);
-  const stack = [];
-  while (head) {
-    stack.push(head.val);
-    head = head.next;
+  var pre = null,
+    cur = head,
+    temp;
+  while (cur) {
+    temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp
   }
-  while (stack.length) {
-    if (!head) {
-      head = new ListNode(stack.pop());
-    } else {
-      let p = head;
-      while (p.next !== null) {
-        p = p.next;
-      }
-      p.next = new ListNode(stack.pop());
-    }
-  }
-  return head;
+  return pre
 };
+
 function ListNode(val) {
   this.val = val;
   this.next = null;
