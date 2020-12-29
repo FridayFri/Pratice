82d999bf46b8b3c6131db31b53a70ad0d09ca469
shengqiwu <shengqiwu@guanmai.cn>
2020-12-30 02:26:04 +0800

    80

---
+++ leetcode/80.removeDuplicates.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function (nums) {
+  for (let i = 2; i < nums.length; i++) {
+    if (nums[i - 2] === nums[i]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]));
