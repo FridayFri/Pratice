66e44db99666c8a85d952409e3a5aa4072d59c6a
shengqiwu <shengqiwu@guanmai.cn>
2021-01-13 20:03:57 +0800

    90

--- leetcode/90.subsetsWithDup.js
+++ leetcode/90.subsetsWithDup.js
@@ -1,6 +1,19 @@
-
-var subsetsWithDup = function(nums) {
-
+var subsetsWithDup = function (nums) {
+  let res = [];
+  nums.sort((a, b) => a - b);
+  let dfs = (t, start) => {
+    console.log(t,start);
+    res.push(t);
+    for (let i = start; i < nums.length; i++) {
+      // 同层重复，跳过
+      if (i != start && nums[i - 1] == nums[i]) continue;
+      t.push(nums[i]);
+      dfs(t.slice(), i + 1);
+      t.pop();
+    }
+  };
+  dfs([], 0);
+  return res;
 };
 
-console.log(subsetsWithDup([1,2,2]))
+console.log(subsetsWithDup([1, 2, 2]));
