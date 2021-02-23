7d7237a6178df71008a633c7609cea585d0ca310
shengqiwu <shengqiwu1@guanmai.cn>
2021-02-23 10:04:02 +0800

    deepClone

--- sourceCode/write/deepclone.js
+++ sourceCode/write/deepclone.js
@@ -1,13 +1,38 @@
-function deepClone(target) {
-  if (target === null) return null;
-  if (typeof target !== 'object') return target;
-  const cloneTarget = Array.isArray(target) ? [] : {};
-  for (let prop in target) {
-    if (target.hasOwnProperty(prop)) {
-      cloneTarget[prop] = deepClone(target[prop]);
+function deepCopy(obj) {
+  // 创建一个新对象
+  let result = {};
+  let keys = Object.keys(obj),
+    key = null,
+    temp = null;
+
+  for (let i = 0; i < keys.length; i++) {
+    key = keys[i];
+    temp = obj[key];
+    // 如果字段的值也是一个对象则递归操作
+    if (temp && typeof temp === "object") {
+      result[key] = deepCopy(temp);
+    } else {
+      // 否则直接赋值给新对象
+      result[key] = temp;
     }
   }
-  return cloneTarget;
+  return result;
+}
+function deepClone(obj) {
+  let result = {};
+  let keys = Object.keys(obj),
+    key = null,
+    temp = null;
+  for (let i = 0; i < keys.length; i++) {
+    key = keys[i];
+    temp = obj[key];
+    if (temp && typeof temp === "object") {
+      result[key] = deepClone(temp);
+    } else {
+      result[key] = temp;
+    }
+  }
+  return result;
 }
 
 const a = {
