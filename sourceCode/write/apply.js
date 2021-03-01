12e2b0e70946d1f4aff3b76acdfa1f667668ba56
shengqiwu <shengqiwu1@guanmai.cn>
2021-03-01 12:57:47 +0800

    apply

---
+++ sourceCode/write/apply.js
@@ -0,0 +1,17 @@
+Function.prototype.mApply = function (context, args) {
+  context = context === null ? window : context;
+  let result;
+  context["fn"] = this;
+  console.log(this);
+  result = context["fn"](...args);
+  delete context["fn"];
+  return result;
+};
+
+const ooo = {
+  name: "symbol",
+};
+function fun1(...args) {
+  console.log(this.name, args);
+}
+console.log(fun1.mApply(ooo, [1, 2]));
