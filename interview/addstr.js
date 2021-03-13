7f99d3fbf89b67db350bc5ec1608ba0ac1d65cf9
shengqiwu <shengqiwu1@guanmai.cn>
2021-03-13 21:07:43 +0800

    addstr

--- interview/addstr.js
+++ interview/addstr.js
@@ -9,22 +9,20 @@ function add(s, t) {
 function add1(a, b) {
   //取两个数字的最大长度
   let maxLength = Math.max(a.length, b.length);
-  //用0去补齐长度
-  a = a.padStart(maxLength, 0); //"294732947329847328947328947382"
-  b = b.padStart(maxLength, 0); //"000000000000000000000000000011"
-  let t = 0,
-    f = 0,
-    sum = "";
+  a = a.padStart(maxLength, 0);
+  b = b.padStart(maxLength, 0);
+  let t = 0;
+  let f = 0;
+  let sum = "";
   for (let i = maxLength - 1; i >= 0; i--) {
-    t = parseFloat(a[i]) + parseFloat(b[i]) + f;
+    t = parseInt(a[i]) + parseInt(b[i]) + f;
     f = Math.floor(t / 10);
+    console.log(sum)
     sum = (t % 10) + sum;
-    console.log(sum,f)
   }
   if (f == 1) {
     sum = "1" + sum;
   }
   return sum;
 }
-
 console.log(add1("294732947329847328947328947382", "21"));
