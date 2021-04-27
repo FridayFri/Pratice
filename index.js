b70bf391148981a5cc2325754e9f39b1d0cbb106
shengqiwu <shengqiwu@codemao.cn>
2021-04-27 23:38:49 +0800

    test

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// function transform(obj){
-//     let res = new Array(12).fill(null)
-//     for(let index in obj){
-//         res[index-1] = obj[index]
-//     }
-//     return res
-// }
+function transform(obj){
+    let res = new Array(12).fill(null)
+    for(let index in obj){
+        res[index-1] = obj[index]
+    }
+    return res
+}
 
-// const obj = {
-//     1:222,
-//     2:123,
-//     5:888
-// }
-// console.log(transform(obj))
+const obj = {
+    1:222,
+    2:123,
+    5:888
+}
+console.log(transform(obj))
 
 // 腾讯云
 var locationList = [
