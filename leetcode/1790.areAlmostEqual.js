2004050a50cec33c1f68d6193602f904ebfb5e09
shengqiwu <shengqiwu1@guanmai.cn>
2021-05-11 00:05:36 +0800

    1790

--- leetcode/1790.areAlmostEqual.js
+++ leetcode/1790.areAlmostEqual.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-05-10 23:38:46
- * @LastEditTime: 2021-05-10 23:55:38
- * @LastEditors: your name
+ * @LastEditTime: 2021-05-11 00:05:14
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /Practice/leetcode/1790.areAlmostEqual.js
  */
@@ -12,7 +12,18 @@
  * @return {boolean}
  */
 var areAlmostEqual = function (s1, s2) {
-    
+  if (s1 === s2) {
+    return true;
+  }
+  let diff = [],
+    len = s1.length;
+  while (len--) {
+    if (s1.charAt(len) !== s2.charAt(len)) {
+        diff.push([s1.charAt(len), s2.charAt(len)])
+    } 
+  }
+  return diff.length === 2 && diff[0][0] === diff[1][1] && diff[0][1] === diff[1][0]
+
 };
 
 console.log(areAlmostEqual("bank", "kanb"));
