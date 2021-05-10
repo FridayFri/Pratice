f986fecc416a5efe70baaecb4bec401a12a0282c
shengqiwu <shengqiwu1@guanmai.cn>
2021-05-10 23:55:44 +0800

    1790

---
+++ leetcode/1790.areAlmostEqual.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-10 23:38:46
+ * @LastEditTime: 2021-05-10 23:55:38
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /Practice/leetcode/1790.areAlmostEqual.js
+ */
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function (s1, s2) {
+    
+};
+
+console.log(areAlmostEqual("bank", "kanb"));
