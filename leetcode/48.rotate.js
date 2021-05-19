24813da82de266d7fdfb6d3ca7b5c8a7470803b6
shengqiwu <shengqiwu@codemao.cn>
2021-05-20 01:44:36 +0800

    48

--- leetcode/48.rotate.js
+++ leetcode/48.rotate.js
@@ -1,6 +1,21 @@
+var rotate = function (matrix) {
+  let res = new Array(matrix.length).fill([]);
+  for (let i = matrix.length - 1; i >= 0; i--) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      let temp = matrix[i][j];
+      console.log(temp);
+      res[j] = res[j].length ? [...res[j], temp] : [temp];
+    }
+  }
 
-var rotate = function(matrix) {
-
+  return res;
 };
 
-console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]))
+console.log(
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+// [[7,4,1],[8,5,2],[9,6,3]]
