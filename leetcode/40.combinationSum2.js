36c163553a28ca5b485371f4b308f2bdcd6d817f
shengqiwu <shengqiwu@codemao.cn>
2021-07-07 01:21:22 +0800

    40

--- leetcode/40.combinationSum2.js
+++ leetcode/40.combinationSum2.js
@@ -1,9 +1,24 @@
 var combinationSum2 = function (candidates, target) {
-    candidates.sort((a,b) => a - b );
-    const res = []
-    const dfs = (start,temp,sum)=>{
-
+  candidates.sort((a, b) => a - b);
+  const res = [];
+  const dfs = (start, temp, sum) => {
+    if (sum >= target) {
+      if (sum == target) {
+        res.push(temp.slice());
+      }
+      return;
     }
+    for (let i = start; i < candidates.length; i++) {
+      if (i - 1 >= start && candidates[i - 1] == candidates[i]) {
+        continue;
+      }
+      temp.push(candidates[i]);
+      dfs(i + 1, temp, sum + candidates[i]);
+      temp.pop();
+    }
+  };
+  dfs(0, [], 0);
+  return res;
 };
 
 console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], (target = 8)));
