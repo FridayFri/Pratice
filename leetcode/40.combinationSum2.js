185090788621df23b430f46e9b2de2f6be4582ea
shengqiwu <shengqiwu@codemao.cn>
2021-07-06 23:45:55 +0800

    40

---
+++ leetcode/40.combinationSum2.js
@@ -0,0 +1,9 @@
+var combinationSum2 = function (candidates, target) {
+    candidates.sort((a,b) => a - b );
+    const res = []
+    const dfs = (start,temp,sum)=>{
+
+    }
+};
+
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], (target = 8)));
