328a2843e8bb60d0e8f3a9106b677e06751b2bd5
shengqiwu <shengqiwu@codemao.cn>
2021-08-12 18:51:47 +0800

    maopao

--- sourceCode/sort/maopao.js
+++ sourceCode/sort/maopao.js
@@ -17,13 +17,11 @@ function maopao(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[i]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
   }
-  return arr
+  return arr;
 }
 const arr = [1, 5, 4, 2, 9, 7, 8];
 console.log(maopao(arr));
