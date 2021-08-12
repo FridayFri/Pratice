d78d3ed8cb3162967df1f202827614fbd150dc9f
shengqiwu <shengqiwu@codemao.cn>
2021-08-12 16:04:37 +0800

    binary

--- sourceCode/sort/binary.js
+++ sourceCode/sort/binary.js
@@ -1,34 +1,34 @@
 function binarySearch(arr, target) {
-    let max = arr.length - 1;
-    let min = 0;
-    while (min <= max) {
-      let mid = Math.floor((max + min) / 2);
-      if (target < arr[mid]) {
-        max = mid - 1;
-      } else if (target > arr[mid]) {
-        min = mid + 1;
-      } else {
-        return mid;
-      }
+  let max = arr.length - 1;
+  let min = 0;
+  while (min <= max) {
+    let mid = Math.floor((max + min) / 2);
+    if (target < arr[mid]) {
+      max = mid - 1;
+    } else if (target > arr[mid]) {
+      min = mid + 1;
+    } else {
+      return mid;
     }
-    return -1;
   }
-  const arr = [1, 5, 4, 2, 9, 7, 8];
-  console.log(binarySearch(arr, 8));
+  return -1;
+}
+const arr = [1, 2, 3, 4, 7, 8];
+// console.log(binarySearch(arr, 8));
 
-  function twoSearch(arr, target) {
-    let max = arr.length - 1;
-    let min = 0;
-    while (min <= max) {
-      let mid = ((max + min) / 2) | 0;
-      if (target < arr[mid]) {
-        max = mid - 1;
-      }else if(target > arr[mid]){
-          min = mid -1
-      }else{
-          return mid
-      }
+function search(arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const mid = parseInt((start + end) / 2);
+    if (arr[mid] === key) {
+      return mid;
+    } else if (key > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
-    return 'target不存在'
   }
-  console.log(twoSearch(arr, 0));
+  return -1;
+}
+console.log(search(arr, 7));
