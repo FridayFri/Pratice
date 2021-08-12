bb4e890aa5d33e92247c2736822e18812c4fb928
shengqiwu <shengqiwu@codemao.cn>
2021-08-12 14:48:32 +0800

    quick

--- sourceCode/sort/1.quickSort.js
+++ sourceCode/sort/1.quickSort.js
@@ -4,7 +4,6 @@ var quickSort = function (arr) {
   }
   let index = Math.floor(arr.length / 2);
   const mid = arr.splice(index, 1)[0];
-  console.log(index, mid, arr[index], arr);
   const left = [];
   const right = [];
   for (let i = 0; i < arr.length; i++) {
@@ -17,5 +16,23 @@ var quickSort = function (arr) {
   return quickSort(left).concat([mid], quickSort(right));
 };
 
+const quick = function (arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const index = Math.floor(arr.length / 2);
+  const mid = arr.splice(index, 1)[0];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < mid) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quick(left).concat(mid, quick(right));
+};
+
 const arr = [85, 24, 63, 45, 17, 31, 96, 50];
-console.log(quickSort(arr));
+console.log(quick(arr));
