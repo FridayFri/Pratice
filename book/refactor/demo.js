09a97bc02bde1d7672822c2c1f0d52fc3e80abe9
shengqiwu <shengqiwu@shein.com>
2022-03-14 22:55:32 +0800

    feat: demo

--- book/refactor/demo.js
+++ book/refactor/demo.js
@@ -1,8 +1,11 @@
 const { players, invoices } = require("./mock");
 function statement(invoice, plays) {
+  const statementData = {};
+  return renderPlainText(statementData, invoice, plays);
+}
+function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
